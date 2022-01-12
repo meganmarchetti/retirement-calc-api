1554f4a31d45b0db81a507fc1a6ee5758ad0b0f2
Megan Marchetti <48037645+meganmarchetti@users.noreply.github.com>
2022-01-12 12:53:26 -0500

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 //process.env.PORT || 3000
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middlewares
 app.use(express.json());
